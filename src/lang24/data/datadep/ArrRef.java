51d872af77f8d5d7f2be0727df060f5fe088d3fd
Marko Z. Muc <marko.muc12@gmail.com>
2024-08-30 17:17:26 +0200

    vec: extended ArrRef structure

--- src/lang24/data/datadep/ArrRef.java
+++ src/lang24/data/datadep/ArrRef.java
@@ -1,32 +1,41 @@
 package lang24.data.datadep;
 
+import lang24.data.ast.tree.defn.AstDefn;
 import lang24.data.ast.tree.expr.AstExpr;
+import lang24.data.ast.tree.expr.AstNameExpr;
 import lang24.data.ast.tree.stmt.AstStmt;
 
 public class ArrRef {
-    public AstExpr subscript_expr;
-    public AstExpr arrExpr;
+    public AstExpr subscriptExpr;
+    public AstNameExpr arrExpr;
     public AstStmt refStmt;
+    public AstDefn arrDefn;
+    public LoopDescriptor loop;
     public int depth;
+    public int stmtNum;
     public boolean assign;
 
-    public ArrRef(AstExpr subscriptExpr, AstExpr arrExpr, AstStmt refStmt, int depth) {
-        this.subscript_expr = subscriptExpr;
+    public ArrRef(AstExpr subscriptExpr, AstNameExpr arrExpr, AstStmt refStmt,
+                  AstDefn arrDefn, LoopDescriptor loop, int stmtNum, int depth) {
+        this.subscriptExpr = subscriptExpr;
         this.arrExpr = arrExpr;
         this.refStmt = refStmt;
+        this.arrDefn = arrDefn;
+        this.loop = loop;
+        this.stmtNum = stmtNum;
         this.depth = depth;
         this.assign = false;
     }
 
     @Override
     public String toString() {
-        String expr = arrExpr.toString();
+        String expr = this.arrExpr.toString() + "[" + this.subscriptExpr.toString() + "]";
         if(this.assign) {
             expr = expr + "=";
         }else{
             expr =  "=" + expr;
         }
 
-        return String.format("%-20s |\tassign=%s", expr, assign);
+        return String.format("%-20s |\tassign=%-5s | L%dS%d", expr, this.assign, this.depth, this.stmtNum);
     }
 }
